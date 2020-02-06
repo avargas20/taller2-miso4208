5510ca947285882c5fb9676c88d0164fab9aa573
avargas20 <47221302+avargas20@users.noreply.github.com>
2020-02-06 01:48:24 -0500

    Cypress punto 1

--- cypress/integration/simple_spec.js
+++ cypress/integration/simple_spec.js
@@ -10,4 +10,33 @@ describe('Los estudiantes login', function() {
       cy.get('.cajaLogIn').contains('Ingresar').click()
       cy.contains('El correo y la contraseña que ingresaste no figuran en la base de datos. Intenta de nuevo por favor.')
     })
+    //login correcto
+    it('Visits los estudiantes and succeds at login', function() {
+        cy.visit('https://losestudiantes.co')
+        cy.contains('Cerrar').click()
+        cy.contains('Ingresar').click()
+        cy.get('.cajaLogIn').find('input[name="correo"]').click().type("vargaspintoadriana@gmail.com")
+        cy.get('.cajaLogIn').find('input[name="password"]').click().type("Avargas1392*")
+        cy.get('.cajaLogIn').contains('Ingresar').click()
+        cy.get('.botonIngresar').should('not.be.visible')
+    })
+})
+
+//Se crea cuenta manualmente
+describe('Los estudiantes registro', function() {
+    it('Visits los estudiantes and fails register', function() {
+        cy.visit('https://losestudiantes.co')
+        cy.contains('Cerrar').click()
+            //Lineas nuevas  
+        cy.contains('Ingresar').click()
+        cy.get('.cajaSignUp > .loginForm').find('input[name="nombre"]').click().type("Adriana")
+        cy.get('.cajaSignUp > .loginForm').find('input[name="apellido"]').click().type("Vargas")
+        cy.get('.cajaSignUp > .loginForm').find('input[name="correo"]').click().type("varca.software@gmail.com")
+        cy.get('.no_bold').click()
+        cy.get('div.jsx-527058112 > .form-group > .has-success > .form-control').select('16')
+        cy.get('.cajaSignUp > .loginForm').find('input[name="password"]').click().type("Avargas1392*")
+        cy.get('label.jsx-520551651').click()
+        cy.get('.cajaSignUp > .loginForm > .logInButton').click()
+        cy.get('.sweet-alert').contains('Ocurrió un error activando tu cuenta')
+    })
 })
