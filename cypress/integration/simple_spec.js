bb72898cd3c6efaa33d2ed117df58761a77bcd96
avargas20 <47221302+avargas20@users.noreply.github.com>
2020-02-06 16:17:00 -0500

    Cypress punto 3

--- cypress/integration/simple_spec.js
+++ cypress/integration/simple_spec.js
@@ -1,17 +1,17 @@
-describe('Los estudiantes login', function() {
+describe('Autenticación en Los estudiantes', function() {
     //login fallido
-    it('Visits los estudiantes and fails at login', function() {
+    it('Login fallido', function() {
       cy.visit('https://losestudiantes.co')
       cy.contains('Cerrar').click()
-      //Lineas nuevas  
       cy.contains('Ingresar').click()
       cy.get('.cajaLogIn').find('input[name="correo"]').click().type("wrongemail@example.com")
       cy.get('.cajaLogIn').find('input[name="password"]').click().type("1234")
       cy.get('.cajaLogIn').contains('Ingresar').click()
       cy.contains('El correo y la contraseña que ingresaste no figuran en la base de datos. Intenta de nuevo por favor.')
     })
+
     //login correcto
-    it('Visits los estudiantes and succeds at login', function() {
+    it('Login correcto', function() {
         cy.visit('https://losestudiantes.co')
         cy.contains('Cerrar').click()
         cy.contains('Ingresar').click()
@@ -23,11 +23,10 @@ describe('Los estudiantes login', function() {
 })
 
 //Se crea cuenta manualmente
-describe('Los estudiantes registro', function() {
-    it('Visits los estudiantes and fails register', function() {
+describe('Registro en Los estudiantes', function() {
+    it('Falla registro', function() {
         cy.visit('https://losestudiantes.co')
         cy.contains('Cerrar').click()
-            //Lineas nuevas  
         cy.contains('Ingresar').click()
         cy.get('.cajaSignUp > .loginForm').find('input[name="nombre"]').click().type("Adriana")
         cy.get('.cajaSignUp > .loginForm').find('input[name="apellido"]').click().type("Vargas")
@@ -41,8 +40,8 @@ describe('Los estudiantes registro', function() {
     })
 })
 
-//Busqueda de profesores
-describe('Búsqueda de profesores', function() {
+//Profesores
+describe('Búsqueda y selección de profesores', function() {
     it('Búsqueda de profesores', function() {
         cy.visit('https://losestudiantes.co')
         cy.contains('Cerrar').click()
@@ -50,14 +49,24 @@ describe('Búsqueda de profesores', function() {
         cy.get('.cajaLogIn').find('input[name="correo"]').click().type("varca.software@gmail.com")
         cy.get('.cajaLogIn').find('input[name="password"]').click().type("Avargas1392*")
         cy.get('.cajaLogIn').contains('Ingresar').click()
-        cy.get('.Select-option-group-label').should('not.be.visible')
-        cy.get('.Select-menu-outer').should('not.be.visible')
+        cy.get('.botonIngresar').should('not.be.visible')
         cy.get('.Select-placeholder').click().type("gloria cortes")
-        cy.get('.Select-option-group-label').should('be.visible')
-        cy.get('.Select-menu-outer').should('be.visible')
         cy.get('.Select-option-group-label').should('contain', 'profesores')
         cy.get('#react-select-required_error_checksum--option-0').should('contain', 'Gloria Cristina Cortes Buitrago - Ingeniería de Sistemas')
         cy.get('#react-select-required_error_checksum--option-0').click()
         cy.url().should('eq', 'https://losestudiantes.co/universidad-de-los-andes/ingenieria-de-sistemas/profesores/gloria-cristina-cortes-buitrago')
+    })
+
+    //Dirigirse a la página de un profesor
+    it('Ir a la página de un profesor', function() {
+        cy.visit('https://losestudiantes.co')
+        cy.contains('Cerrar').click()
+        cy.contains('Ingresar').click()
+        cy.get('.cajaLogIn').find('input[name="correo"]').click().type("varca.software@gmail.com")
+        cy.get('.cajaLogIn').find('input[name="password"]').click().type("Avargas1392*")
+        cy.get('.cajaLogIn').contains('Ingresar').click()
+        cy.get('.botonIngresar').should('not.be.visible')
+        cy.get('div.jsx-1590631759.btn-group').should('contain', 'Alfabético').click()
+        cy.get('div.jsx-633353764.profesor').contains('Alejandro Hernandez Arboleda').click()
     })
 })
